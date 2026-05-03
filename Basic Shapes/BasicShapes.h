be7589b07fc39ff6b4bc002f6620691051910a6d
emsample <esamplebox@gmail.com>
2026-05-03 16:51:44 -0400

    Starting with my base Basic Shapes class

--- Basic Shapes/BasicShapes.h
+++ Basic Shapes/BasicShapes.h
@@ -5,3 +5,23 @@
 using namespace std;
 
 class BasicShape
+{
+	private:
+		double area;
+		string name;
+
+	public:
+		BasicShape();
+
+		double getArea() const;
+		string getName() const;
+
+		void setArea(double a);
+		void setName(string n);
+
+		virtual void calcArea() = 0;
+
+		virtual ~BasicShape();
+};
+
+#endif
