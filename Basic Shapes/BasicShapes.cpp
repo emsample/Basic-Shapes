e4e5605ba1e6b6941621d57084918956a041d3e3
Erich <espamdumpster@gmail.com>
2026-05-03 20:11:08 -0400

    Basic shape implementation is simple

--- Basic Shapes/BasicShapes.cpp
+++ Basic Shapes/BasicShapes.cpp
@@ -1,3 +1,25 @@
 #include "BasicShapes.h"
 
-BasicShape::
+BasicShape::BasicShape()
+{
+	area = 0.0;
+	name = " ";
+}
+double BasicShape::getArea() const
+{
+	return area;
+}
+string BasicShape::getName() const
+{
+	return name;
+}
+void BasicShape::setArea(double a)
+{
+	area = a;
+}
+void BasicShape::setName(string n)
+{
+	name = n;
+}
+BasicShape::~BasicShape()
+{ }
