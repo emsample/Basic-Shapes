10d01b6e69b1ba4976759acc9157f4621a9eff19
emsample <esamplebox@gmail.com>
2026-05-04 15:30:30 -0400

    Finsished the constructor, did the getters and setters and the Area formula

--- Basic Shapes/Rectangle.cpp
+++ Basic Shapes/Rectangle.cpp
@@ -3,5 +3,28 @@
 Rectangle::Rectangle(double l, double w, string n)
 {
 	length = l;
+	width = w;
 
+	setName(n);
+	calcArea();
+}
+double Rectangle::getLength() const
+{
+	return length;
+}
+double Rectangle::getWidth() const
+{
+	return width;
+}
+void Rectangle::setLength(double l)
+{
+	length = l;
+}
+void Rectangle::setWidth(double w)
+{
+	width = w;
+}
+void Rectangle::calcArea()
+{
+	setArea(length * width);
 }
