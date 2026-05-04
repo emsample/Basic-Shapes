ca02148c27d4153d19399255f413b13e427e4fd9
emsample <esamplebox@gmail.com>
2026-05-04 15:22:47 -0400

    Finished my rectangle header and have started writing the constructor

---
+++ Basic Shapes/Rectangle.cpp
@@ -0,0 +1,7 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(double l, double w, string n)
+{
+	length = l;
+
+}
---
+++ Basic Shapes/Rectangle.h
@@ -0,0 +1,25 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "BasicShapes.h"
+
+class Rectangle : public BasicShape
+{
+private:
+
+	double length;
+	double width;
+
+public:
+
+	Rectangle(double l, double w, string n = "Circle");
+
+	double getLength() const;
+	double getWidth() const;
+
+	void setLength(double l);
+	void setWidth(double w);
+
+	virtual void calcArea();
+};
+#endif // !RECTANGLE_H
